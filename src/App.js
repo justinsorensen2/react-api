7552121ab1b7f71c56b3446b88e78cde07d13eb4
justinsorensen2dotnet <>
2020-03-23 09:10:33 -0400

    repaired api

--- src/App.js
+++ src/App.js
@@ -1,79 +1,14 @@
 import React, { Component } from 'react'
 import Header from './components/Header'
-import axios from 'axios'
-var img = 'https://image.tmdb.org/t/p/w500'
+import Movie from './components/Movie'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      movies: [
-        {
-          id: 666,
-          title: 'Bob Smith Goes to Hell',
-          poster_path: '/4p1N2Qrt8j0H8xMHMHvtRxv9weZ.jpg',
-          overview: 'Sometimes people get what they deserve.',
-          url: '',
-          alt: '',
-        },
-        {
-          id: 89,
-          title: 'Indiana Jones and the Last Crusade',
-          poster_path: '/4p1N2Qrt8j0H8xMHMHvtRxv9weZ.jpg',
-          overview:
-            'When Dr. Henry Jones Sr. suddenly goes missing while pursuing the Holy Grail, eminent archaeologist Indiana must team up with Marcus Brody, Sallah and Elsa Schneider to follow in his fathers footsteps and stop the Nazis from recovering the power of eternal life.',
-          url: '',
-          alt: '',
-        },
-      ],
-    }
-  }
-
-  componentDidMount() {
-    // once the app is added to the page, make an API call to load the data
-    axios
-      .get(
-        'https://api.themoviedb.org/3/discover/movie?primary_release_year=1989&sort_by=popularity.desc&api_key=16dccf4635fc67bb4bf1e38c0a4459dd'
-      )
-      .then(res => {
-        const movies = res.data
-        this.state.movies.url = `https://image.tmdb.org/t/p/w500${this.state.movies.poster_path}`
-        this.state.movies.alt = `${this.state.movies.title} poster`
-        this.setState({ movies })
-      })
-  }
-
   render() {
-    const movieList = this.state
     return (
       <>
         <Header></Header>
         <main>
-          <ul>
-            {this.state.map(movie, index => {
-              return (
-                <li key={movie[index].id}>
-                  <div className="card">
-                    <div className="card-body">
-                      <h5 className="card-title">
-                        ID:{movie.id} Title:
-                        {movie[index].movies.title}
-                      </h5>
-                      <article className="card-image">
-                        <img
-                          src={this.state[index].movies.url}
-                          alt={this.state[index].movies.alt}
-                        />
-                      </article>
-                      <p className="card-text">
-                        {this.state[index].movies.overview}
-                      </p>
-                    </div>
-                  </div>
-                </li>
-              )
-            })}
-          </ul>
+          <Movie></Movie>
         </main>
       </>
     )
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,23 +1,53 @@
 import React, { Component } from 'react'
 
 class Movie extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      movieList: [],
+    }
+  }
+
+  componentDidMount() {
+    // once the app is added to the page, make an API call to load the data
+    fetch(
+      'https://api.themoviedb.org/3/discover/movie?primary_release_year=1989&sort_by=popularity.desc&api_key=16dccf4635fc67bb4bf1e38c0a4459dd'
+    )
+      .then(response => {
+        // console.log(response)
+        return response.json()
+      })
+      .then(movie => {
+        this.setState({
+          movieList: movie.results,
+        })
+      })
+  }
   render() {
-    let url = `https://image.tmdb.org/t/p/w500${this.props.poster_path}`
     let alt = `${this.props.title} poster`
     return (
-      <li key={this.props.id}>
-        <div className="card">
-          <div className="card-body">
-            <h5 className="card-title">
-              ID:{this.props.id} Title:{this.props.title}
-            </h5>
-            <article className="card-image">
-              <img src={url} alt={alt} />
-            </article>
-            <p className="card-text">{this.props.overview}</p>
-          </div>
-        </div>
-      </li>
+      <ul>
+        {this.state.movieList.map(movie => {
+          return (
+            <li key={movie.id}>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">
+                    ID:{movie.id} Title:
+                    {movie.title}
+                  </h5>
+                  <article className="card-image">
+                    <img
+                      src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${movie.poster_path}`}
+                    />
+                  </article>
+                  <p className="card-text">{movie.overview}</p>
+                </div>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     )
   }
 }
